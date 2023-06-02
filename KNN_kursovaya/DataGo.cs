f253bf713dbee0e15d079c68e6f13b92d98faba5
Alexanderzxc <yaaa.22@uni-dubna.ru>
2023-06-02 12:03:57 +0300

    first comm

---
+++ KNN_kursovaya/DataGo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNN_kursovaya
+{
+    internal class DataGo
+    {
+        private List<string> datafromcsv = new List<string>();
+        private List<string[]> data = new List<string[]>();
+
+        public List<string[]> Data { get => data; }
+
+        public void get_data()
+        {
+            using (TextFieldParser tfp = new TextFieldParser(@"C:\Users\sasha\RiderProjects\KNN_kursovaya\new.csv"))
+            {
+                tfp.TextFieldType = FieldType.Delimited;
+                tfp.SetDelimiters(",");
+
+                while (!tfp.EndOfData)
+                {
+                    datafromcsv.Add(tfp.ReadLine());
+                }
+
+                foreach (var datas in datafromcsv)
+                {
+                    string[] wo = datas.Split(",");
+                    data.Add(wo);
+                }
+            }
+
+        }
+    }
+}
---
+++ KNN_kursovaya/Object.cs
@@ -0,0 +1,97 @@
+﻿using KNN_kursovaya;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Введите радиус опухоли, периметр опухоли, количество соседей");
+double rad = Convert.ToDouble(Console.ReadLine());
+double per = Convert.ToDouble(Console.ReadLine());
+int k = Convert.ToInt32(Console.ReadLine());
+
+Object ob = new Object(rad, per,k);
+
+DataGo dataGo = new DataGo();
+dataGo.get_data();
+
+ob.GetDist(dataGo.Data);
+
+ob.Predict();
+
+ob.Answer();
+
+
+public class Object
+{
+    private double radius, texture, k;
+
+    private List<string[]> distance = new List<string[]>();
+
+    private List<string> diagnoses = new List<string>();
+
+    public Object(double radius, double texture, int k)
+    {
+        this.radius = radius;
+        this.texture = texture;
+        this.k = k;
+    }
+
+    public void Answer()
+    {
+        int count_B=0, count_M=0;
+        foreach (string s in diagnoses)
+        {
+            if (s == "B")
+            {
+                count_B++;
+            }
+            else
+            {
+                count_M++;
+            }
+        }
+        if (count_B > count_M)
+        {
+            Console.WriteLine("Ваш диагноз доброкачественная опухоль");
+        }
+        else
+        {
+            Console.WriteLine("Ваш диагноз злокачественная опухоль");
+        }
+    }
+
+    public void Predict()
+    {
+        for (int j = 0; j < k; j++)
+        {
+            double min = 100;
+            int index = 0;
+            for (int i = 0; i < distance.Count; i++)
+            {
+                if (Convert.ToDouble(distance[i][1]) < min)
+                {
+                    min = Convert.ToDouble(distance[i][1]);
+                    index = i;
+                }
+            }
+            diagnoses.Add(distance[index][0]);
+            distance.RemoveAt(index);
+        }
+    }
+
+    public void GetDist(List<string[]> q)
+    {
+        double d_1, d_2;
+        for (int i = 0; i < q.Count; i++) 
+        {
+            string[] doubles = new string[2];
+            d_1 = Math.Pow(radius - Convert.ToDouble(q[i][1].Replace(".",",")), 2);
+            d_2 = Math.Pow(texture - Convert.ToDouble(q[i][2].Replace(".",",")), 2);
+            string b = Convert.ToString(Math.Sqrt(d_1 + d_2));
+            for (int j = 0; j < 2; j++)
+            {
+                doubles[0] = q[i][0];
+                doubles[1] = b;
+            }
+            distance.Add(doubles);
+        }
+    }
+
+}
